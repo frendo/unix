1c448fade0d3fdf5393c1d4c964c54cd91aa71e3
frendo <frendo@users.noreply.github.com>
2016-10-15 10:57:49 +0100

    Create interface.sh

---
+++ interface.sh
@@ -0,0 +1,11 @@
+#!/bin/bash                                                                                                                                 
+                                                                                   
+if [ $1 == 'wifi' ]                                                                
+then                                                                               
+        echo ....changing interfaces to wifi configuration                         
+        sudo cp /etc/network/interfaces-wifi /etc/network/interfaces               
+else                                                                               
+        echo ...changing interfaces to adhoc configuration                         
+        sudo cp /etc/network/interfaces-adhoc /etc/network/interfaces              
+                                                                                   
+fi  
